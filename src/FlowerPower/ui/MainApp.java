156545eb2965b28877d3d53f34cbfdba2b9a1490
Maren <MGood8086@gmail.com>
2026-04-24 11:36:02 -0500

    it shows up in screen now :)

--- src/FlowerPower/ui/MainApp.java
+++ src/FlowerPower/ui/MainApp.java
@@ -19,40 +19,48 @@ public class MainApp {
     //variables
     Gameboard game;
     public static final double CANVAS_SIZE = 256;
+    public static final double TILE_SIZE = 32;
     private CanvasWindow canvas;
     private Color backgroundColor = new Color(34, 125, 73);
     Explorer explorer;
-
-
-    private MainApp(){
+    public Rectangle explorerIcon;
+ 
+    private MainApp() {
         //constructor
         // add obstacles, explorer, collectables OR have Gameboard.java implement those and then just add a new Gameboard object
         canvas = new CanvasWindow("FlowerPower", (int) CANVAS_SIZE, (int) CANVAS_SIZE);
         canvas.setBackground(backgroundColor);
         game = new Gameboard(16, 16); // 256 by 256 for full gameboard
-        explorer = new Explorer(null, game);
-        //canvas.add(game.getBoard());
+        explorer = new Explorer(new GraphicsGroup(), game);
+        explorerIcon = new Rectangle(0, 0, (int) TILE_SIZE, (int) TILE_SIZE);
+        explorerIcon.setFillColor(Color.WHITE);
+        explorer.icon.add(explorerIcon); 
     }
 
     public static void main(String[] args) {
         new MainApp().play();
     }
 
-    private void play(){
+    private void play() {
         // idk if we need this but just setting things up; we do
         setUpGame();
     }
 
     private void setUpGame() {
         //this will set up and reset the layout for the beginning of a round.
-
+        canvas.removeAll();
+        //adding to canvas
+        canvas.add(game.getBoard());
+        canvas.add(explorer.icon);
         // explorer movement
         canvas.onKeyDown(key -> {
             switch (key.getKey()) {
-                case UP_ARROW    -> explorer.moveUp();
-                case DOWN_ARROW  -> explorer.moveDown();
-                case LEFT_ARROW  -> explorer.moveLeft();
-                case RIGHT_ARROW -> explorer.moveRight();
+                case UP_ARROW: explorer.moveUp();
+                case DOWN_ARROW: explorer.moveDown();
+                case LEFT_ARROW: explorer.moveLeft();
+                case RIGHT_ARROW: explorer.moveRight();
+                default:
+                    break;
             }
         });
     }
