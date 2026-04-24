b1b9173b7f9ac22ea5f2738ea5a07f9117fe62ed
Maren <MGood8086@gmail.com>
2026-04-23 21:03:59 -0500

    started on ui

--- src/FlowerPower/ui/MainApp.java
+++ src/FlowerPower/ui/MainApp.java
@@ -1,9 +1,13 @@
 package FlowerPower.ui;
 
+import java.awt.Color;
 import FlowerPower.model.Collectible;
 import FlowerPower.model.Obstacle;
 import FlowerPower.model.Explorer;
 import FlowerPower.model.Gameboard;
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.Rectangle;
 
 //Maren is doing ui
 /**
@@ -14,11 +18,18 @@ public class MainApp {
 
     //variables
     Gameboard game;
+    public static final double CANVAS_SIZE = 256;
+    private CanvasWindow canvas;
+    private Color backgroundColor = new Color(34, 125, 73);
+
 
     private MainApp(){
         //constructor
         // add obstacles, explorer, collectables OR have Gameboard.java implement those and then just add a new Gameboard object
+        canvas = new CanvasWindow("FlowerPower", (int) CANVAS_SIZE, (int) CANVAS_SIZE);
+        canvas.setBackground(backgroundColor);
         game = new Gameboard(16, 16); // 256 by 256 for full gameboard
+        //canvas.add(game.getBoard());
     }
 
     public static void main(String[] args) {
@@ -26,7 +37,13 @@ public class MainApp {
     }
 
     private void play(){
-        // idk if we need this but just setting things up
+        // idk if we need this but just setting things up; we do
+        setUpGame();
+    }
+
+    private void setUpGame() {
+        //this will set up and reset the layout for the beginning of a round.
+
     }
 
 }
